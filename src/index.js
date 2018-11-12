370779bb217ba5754a5aa806671dd05d4ed5a6e1
Sergio Andres <sergio.andres@itequia.com>
2018-11-12 10:27:45 +0100

    last commit

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ const output = document.querySelector('output')
 const input = document.querySelector('input')
 const select = document.querySelector('select')
 
-
 const API_URL = `https://jsonplaceholder.typicode.com/albums`
 
 const id$ = fromEvent(input, 'input').pipe(
 	map(e => e.target.value),
 	filter(id => id>0 && id<10),
 	debounceTime(3000)
-
 )
 
 const resourceType$ = fromEvent(select, 'change').pipe(
@@ -32,4 +30,3 @@ function getAlbums([id, resource]) {
 function render(data) {
 	output.textContent = JSON.stringify(data.response)
 }
-
